e4eebad6d4b9e7dce2ab85af58a820016addc17b
sohit7765 <79397452+sohit7765@users.noreply.github.com>
2021-10-31 11:58:23 +0530

    Add files via upload

---
+++ cal.java
@@ -0,0 +1,137 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class cal {
+
+	private JFrame frame;
+	private JTextField t1;
+	private JTextField t2;
+	private JTextField t3;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					cal window = new cal();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public cal() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("FIRST NUMBER");
+		lblNewLabel.setBackground(Color.WHITE);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel.setBounds(16, 11, 128, 27);
+		frame.getContentPane().add(lblNewLabel);
+		
+		t1 = new JTextField();
+		t1.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		t1.setBounds(175, 14, 128, 20);
+		frame.getContentPane().add(t1);
+		t1.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("SECOND NUMBER");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel_1.setBounds(16, 65, 128, 20);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		t2 = new JTextField();
+		t2.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		t2.setBounds(175, 65, 128, 22);
+		frame.getContentPane().add(t2);
+		t2.setColumns(10);
+		
+		t3 = new JTextField();
+		t3.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		t3.setBounds(215, 142, 73, 39);
+		frame.getContentPane().add(t3);
+		t3.setColumns(10);
+		
+		JButton btnNewButton = new JButton("ADDITION");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int n,m,c;
+				n=Integer.parseInt(t1.getText());
+				m=Integer.parseInt(t2.getText());
+				c=m+n;
+				t3.setText(Integer.toString(c));
+			}
+		});
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnNewButton.setBounds(16, 108, 128, 23);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("SUBTRACTION");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int n,m,c;
+				n=Integer.parseInt(t1.getText());
+				m=Integer.parseInt(t2.getText());
+				c=m-n;
+				t3.setText(Integer.toString(c));
+			}
+		});
+		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnNewButton_1.setBounds(16, 142, 128, 23);
+		frame.getContentPane().add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("MULTIPLICATION");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int n,m,c;
+				n=Integer.parseInt(t1.getText());
+				m=Integer.parseInt(t2.getText());
+				c=m*n;
+				t3.setText(Integer.toString(c));
+			}
+		});
+		btnNewButton_2.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnNewButton_2.setBounds(16, 180, 128, 23);
+		frame.getContentPane().add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("DIVISION");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int n,m,c;
+				n=Integer.parseInt(t1.getText());
+				m=Integer.parseInt(t2.getText());
+				c=m/n;
+				t3.setText(Integer.toString(c));
+			}
+		});
+		btnNewButton_3.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnNewButton_3.setBounds(16, 214, 128, 23);
+		frame.getContentPane().add(btnNewButton_3);
+	}
+}
